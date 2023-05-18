bf9220288b7ffc7fb5534c2c3e5dba7adf53d2fd
Achxter <hansphilemon@gmail.com>
2023-05-18 14:33:23 +0700

    first init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home"
+import About from "./Pages/About";
+// import FAQ from "./Pages/FAQ";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return ( 
+    <div>
+      <Navbar/>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <nav className='nav'>
+        <Link to='/' className='title'>Astoon</Link>
+        <ul>
+            <CustomLink to='/About' className='side'>About</CustomLink>
+            {/* <CustomLink to='/FAQ' className='side'>FAQ</CustomLink> */}
+        </ul>
+    </nav>
+  )
+}
+
+function CustomLink({ to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
+
+export default Navbar
